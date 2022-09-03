ebd7da9b3c7d9ae9f2f7bee1bbab4182ff2929be
Misha Kochan <mk_sysstem@mail.ru>
2022-09-03 19:56:48 +0300

    expirement in args 2

--- app/models.py
+++ app/models.py
@@ -61,13 +61,15 @@ class User(UserMixin, db.Model):
         return check_password_hash(self.password_hash, password)
 
     def launch_task(self, name, description, **kwargs):
-        print(kwargs.items())
-        # rq_job = current_app.task_queue.enqueue('app.new_tasks.' + name, self.id, *args, **kwargs)
-        # task = Task(id=rq_job.get_id(), name=name, description=description,
-        #             user=self.id)
-        # db.session.add(task)
-        # current_app.logger.info(f"task id: {task.id} - add to db")
-        # return task
+        # print(kwargs.items())
+        rq_job = current_app.task_queue.enqueue('app.new_tasks.' + name, self.id,
+                                                path=kwargs['path'],
+                                                job_timeout=kwargs['job_timeout'])
+        task = Task(id=rq_job.get_id(), name=name, description=description,
+                    user=self.id)
+        db.session.add(task)
+        current_app.logger.info(f"task id: {task.id} - add to db")
+        return task
 
     def get_tasks_in_progress(self):
         return Task.query.filter_by(user=self, complete=False).all()

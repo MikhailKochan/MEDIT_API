19189ad4ce2dde44eb33b4ec527f2e4580f768de
MikhailKochan <93484529+MikhailKochan@users.noreply.github.com>
2022-10-05 20:23:55 +0300

    del init_app in celery

--- app/__init__.py
+++ app/__init__.py
@@ -59,9 +59,9 @@ def create_app(config_class=Config):
 
     # sentry.init_app(app)
 
-    # from app.view import Medit
-    # Med = Medit()
-    # Med.init_app(app)
+    from app.view import Medit
+    Med = Medit()
+    Med.init_app(app)
 
     from app.errors import bp as errors_bp
     app.register_blueprint(errors_bp)

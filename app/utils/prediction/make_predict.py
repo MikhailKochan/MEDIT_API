d19ccb84cba260f62fbec44a5fc80b3ed6315300
MikhailKochan <93484529+MikhailKochan@users.noreply.github.com>
2022-10-05 20:02:39 +0300

    add import torch in make predict

--- app/utils/prediction/make_predict.py
+++ app/utils/prediction/make_predict.py
@@ -17,6 +17,9 @@ from app.models import Config
 
 
 def make_predict(image, predict, medit, job=None):
+    import torch
+    torch.multiprocessing.set_start_method('spawn')
+
     if job:
         from app.utils.celery import _set_celery_task_progress as _set_task_progress
     else:

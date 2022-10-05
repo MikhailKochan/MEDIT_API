cd58e1e3ef77567fc80a19e18ea32e51e5516e14
MikhailKochan <93484529+MikhailKochan@users.noreply.github.com>
2022-10-05 21:12:56 +0300

    change multproc in make predict

--- app/utils/prediction/make_predict.py
+++ app/utils/prediction/make_predict.py
@@ -17,9 +17,9 @@ from app.models import Config
 
 
 def make_predict(image, predict, medit, job=None):
-    # import torch
+    import torch
     # torch.multiprocessing.set_start_method('spawn')
-
+    torch.cuda.empty_cache()
     if job:
         from app.utils.celery import _set_celery_task_progress as _set_task_progress
     else:
@@ -102,7 +102,7 @@ def make_predict(image, predict, medit, job=None):
 
                 outputs = predictor(im)
 
-                outputs = outputs["instances"].to("cpu")
+                outputs = outputs["instances"].to(cfg.MODEL.DEVICE)
 
                 classes = outputs.pred_classes.tolist() if outputs.has("pred_classes") else None
 

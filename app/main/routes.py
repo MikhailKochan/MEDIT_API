70d113681b61c80356ab9b4d11ef411ed560912d
MikhailKochan <93484529+MikhailKochan@users.noreply.github.com>
2022-12-04 21:43:49 +0300

    del bootstrap wtfforms

--- app/main/routes.py
+++ app/main/routes.py
@@ -21,6 +21,8 @@ def history():
     form = SearchPredictForm()
 
     page = request.args.get('page', 1, type=int)
+    
+    data = {}
 
     if form.validate_on_submit():
         analysis_number = form.data

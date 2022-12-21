17b944a3a30e063e26f0689aabf2d9920977c87f
MikhailKochan <93484529+MikhailKochan@users.noreply.github.com>
2022-12-21 19:41:25 +0300

    change Visualisator to my draw predict test3

--- app/utils/prediction/make_predict.py
+++ app/utils/prediction/make_predict.py
@@ -17,7 +17,7 @@ from config import Config
 from app.celery_task.async_test import quality_checking_image, quality_predict_area, draw_predict
 
 
-def make_predict_test(image, predict, medit, job):
+def make_predict_test(image, predict, medit, job=None):
 
     from rq import get_current_job
     from app.new_tasks import _set_task_progress
